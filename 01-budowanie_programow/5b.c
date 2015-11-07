70ec345257d96241c1876c3ddeea70ae8733f148
Wlodek Bzyl <matwb@ug.edu.pl>
2015-11-07 10:01:43 +0100

    add 5b.c

---
+++ 01-budowanie_programow/5b.c
@@ -0,0 +1,20 @@
+/* Program ten dla dwóch liczb wczytanych z klawiatury
+   wypisuje, która z nich jest większa. */
+
+#include <stdio.h>
+
+int main() {
+  int a, b;
+  char *msg = "Większa liczba to: %d\n";
+
+  printf("Podaj pierwsza liczbe: ");
+  scanf("%d", &a);
+  printf("Podaj pierwsza liczbe: ");
+  scanf("%d", &b);
+
+  if (a > b) {
+     printf(msg, a);
+  } else {
+     printf(msg, b);
+  }
+}
