11e4846d1e9d5c22b0a893ed1b5e1d37abaa5177
Wlodek Bzyl <matwb@univ.gda.pl>
2015-11-07 10:30:07 +0100

    update

--- 01-budowanie_programow/5a.c
+++ 01-budowanie_programow/5a.c
@@ -2,6 +2,14 @@
    według wzoru: rad = 2 * PI * deg / 360    */
 
 #include <stdio.h>
+
+// Math constant PI value in C
+//   http://stackoverflow.com/questions/9912151/math-constant-pi-value-in-c
+
+#define M_PI 3.14159265358979323846
+
+// #define __USE_MISC
+
 #include <math.h>
 
 int main() {
@@ -10,6 +18,6 @@ int main() {
    printf("Podaj kat w stopniach : ");
    scanf("%lf", &deg);
 
-   rad = 2 * 3.14159265358979323846 * deg / 360;
+   rad = 2 * M_PI * deg / 360;
    printf("%.4f deg = %.4f rad\n", deg, rad);
 }
